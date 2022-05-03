231ae197b1c8d0853f50b61ee05eef68a54daeeb
JoseVel4zquez <76269358+JoseVel4zquez@users.noreply.github.com>
2022-05-03 00:43:37 -0500

    otra mamada

--- AStar/AStar.cs
+++ AStar/AStar.cs
@@ -10,6 +10,7 @@ namespace AStar
 {
     class AStar : Nodo
     {
+        
         public Nodo inicio;
         public Nodo meta;
         public Nodo calcHeu;
@@ -31,84 +32,56 @@ namespace AStar
             camino = new List<Nodo>();
 
         }
-        
+
         //------------------------------------------------
         public void empezar()
-		{
+        {
+            
+            var heu= calcular_h(inicio);
+            meta.h = calcular_h(meta);
             Open.Add(inicio);
-			while (termiado!=true)
+            double distancia = Distancia();
+            
+            while (Open.Count>0) {
+                for (int i = 0; i < Open.Count; i++)
+                {
+                    inicio.g = i;
+                    inicio.h = heu;
+                }
+
+                Closed.Add(inicio);
+                if (inicio.f() == meta.f())
+                {
+                    
+                    MessageBox.Show("Se encontro la meta ");
+                    break;
+                }
+
+            }
+		}
+
+        public void insertarNodo(double[] peso)
+		{
+            Nodo n = new Nodo();
+            //n.hijo = peso[];
+
+			if (padre ==null)
 			{
-				if (Open.Count > 0)
-				{
-                    var ganador = 0; //índie o posición dentro del array openset del ganador
-
-                    //evaluamos que OpenSet tiene un menor coste / esfuerzo
-                    for (int i = 0; i < Open.Count; i++)
-                    {
-                        if (Open[i].f < Open[ganador].f)
-                        {
-                            ganador = i;
-
-                        }
-
-                        
-
-                    }
-                    var actual = Open[ganador];
-					if (actual == meta)
-					{
-                        var temporal = actual;
-                        camino.Add(temporal);
-
-						while (temporal.padre != null)
-						{
-                            temporal = temporal.padre;
-                            camino.Add(temporal);
-						}
-                        MessageBox.Show("Ya jalo");
-                        termiado = true;
-					}
-					else
-					{
-                        Closed.Add(actual);
-                        var vecinos = actual.hijo;
-						
-						for (int i =0; i < vecinos.Length;i++)
-						{
-                            var vecino = vecinos[i];
-							if (!Closed.Contains(vecino) && tablero.CurrentCell.Style.BackColor != Color.Black)
-							{
-                                var tempG = actual.g + 1;
-								if (Open.Contains(vecino))
-								{
-                                    if(tempG < vecino.g)
-									{
-                                        vecino.g = tempG;
-									}
-								}
-								else
-								{
-                                    vecino.g = tempG;
-                                    Open.Add(vecino);
-								}
-                                vecino.h = calcular_h(vecino,meta);
-                                vecino.f = vecino.g + vecino.h;
-
-                                vecino.padre = actual;
-                            }
-						}
-
-                        
-                    }
-
-				}
-				else
-				{
-                    MessageBox.Show("No hay caminos");
-                    termiado = true;
-				}
-				
+                padre = n;
 			}
 		}
+
+        public double Distancia()
+		{
+            double restax = meta.X - inicio.X;
+            double xcuadradas = Math.Pow(restax,2);
+            double ycuadradas = Math.Pow((meta.Y - inicio.Y), 2);
+            double res = Math.Sqrt((xcuadradas+ycuadradas));
+
+            return res;
+		}
+
+       
+		
     }
 }
